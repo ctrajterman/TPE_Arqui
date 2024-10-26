7b5cafded977bedbccacc819c4fc2069092fe624
belupetri <belupetri01@gmail.com>
2024-10-26 17:13:13 -0300

    Exceptions update

--- Kernel/exceptions.c
+++ Kernel/exceptions.c
@@ -1,13 +1,23 @@
 
 #define ZERO_EXCEPTION_ID 0
+#define OP_CODE_INVALID_EXCEPTION 6
 
 static void zero_division();
+static void op_code_invalid();
+
 
 void exceptionDispatcher(int exception) {
 	if (exception == ZERO_EXCEPTION_ID)
 		zero_division();
+	if(exception==OP_CODE_INVALID_EXCEPTION)
+		op_code_invalid();
 }
+
 
 static void zero_division() {
 	// Handler para manejar excepcíon
+}
+
+static void op_code_invalid(){
+	// Handler para manejar la excepcion 
 }
